7e161b7f5769fe557df70e3928291d46e4398d95
xMalware <xmalware2@gmail.com>
2017-04-30 01:01:36 +0200

    fix

--- src/fr/badblock/bukkit/auth/runnables/SendRunnable.java
+++ src/fr/badblock/bukkit/auth/runnables/SendRunnable.java
@@ -1,8 +1,5 @@
 package fr.badblock.bukkit.auth.runnables;
 
-import java.util.UUID;
-
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -10,18 +7,19 @@ import fr.badblock.bukkit.auth.AuthPlugin;
 import fr.badblock.gameapi.players.BadblockPlayer;
 
 public class SendRunnable extends BukkitRunnable {
-	private UUID uniqueId;
 	
-	public SendRunnable(Player player){
-		this.uniqueId = player.getUniqueId();
-		runTaskTimer(AuthPlugin.getInstance(), 0, 60L);
+	private Player bukkitPlayer;
+	
+	public SendRunnable(Player bukkitPlayer){
+		this.bukkitPlayer = bukkitPlayer;
+		runTaskTimer(AuthPlugin.getInstance(), 20L, 60L);
 	}
 	
 	@Override
 	public void run(){
-		BadblockPlayer player = (BadblockPlayer) Bukkit.getPlayer(uniqueId);
+		BadblockPlayer player = (BadblockPlayer) bukkitPlayer;
 		
-		if(player == null) {
+		if(player == null || !player.isOnline()) {
 			cancel();
 		} else player.sendPlayer("lobby");
 	}
